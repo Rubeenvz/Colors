f789164f499c13160707d37e179a2ca824950afa
Hector Ruben Vazquez <rubeenvz@outlook.com>
2020-12-22 15:44:17 -0600

    Project structure has been added, etc.
    Project structure has been added, etc.

---
+++ components/general/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Colors</title>
+    <meta name="description" content="Awesome colors!!!">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://rubeenvz.com/colors">
+    <meta property="og:title" content="Colors">
+    <meta property="og:image" content="./src/img.png">
+    <meta property="og:description" content="Awesome colors!!!">
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://rubeenvz.com/colors">
+    <meta property="twitter:title" content="Colors">
+    <meta property="twitter:description" content="Awesome colors!!!">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="./css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="general-container" id="app">
